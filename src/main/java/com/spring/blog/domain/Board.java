b0c8eda5914f88cd5ee7d83d8f331ddca2f1de1a
namtndus <rnrmf0721@gmail.com>
2024-12-02 20:32:28 +0900

    feat: Board 엔티티 속성 추가

--- src/main/java/com/spring/blog/domain/Board.java
+++ src/main/java/com/spring/blog/domain/Board.java
@@ -3,13 +3,17 @@ package com.spring.blog.domain;
 import jakarta.persistence.*;
 
 @Entity
-public class Subject {
+public class Board {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
+    private Integer countArticle;
 
     // 회원이 탈퇴 될때, 회원이 작성한 글과 카테고리도 삭제되길 원함(주체)
     @ManyToOne
