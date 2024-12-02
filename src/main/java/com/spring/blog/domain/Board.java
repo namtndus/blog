e012666cd723c4375a27ab57ef7cf1a7a3fa5ac4
namtndus <rnrmf0721@gmail.com>
2024-12-02 21:13:47 +0900

    fix: Board 엔티티 연관 관계 삭제

--- src/main/java/com/spring/blog/domain/Board.java
+++ src/main/java/com/spring/blog/domain/Board.java
@@ -15,10 +15,4 @@ public class Board {
     @Column(nullable = false)
     private Integer countArticle;
 
-    // 회원이 탈퇴 될때, 회원이 작성한 글과 카테고리도 삭제되길 원함(주체)
-    @ManyToOne
-    @JoinColumn(name = "users_id")
-    private Users users;
-
-
 }
