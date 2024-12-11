d8e01baf720a2816a808f40929d58c559d185d6f
namtndus <rnrmf0721@gmail.com>
2024-12-11 09:34:14 +0900

    feat: Category 엔티티 연관관계 생성

--- src/main/java/com/spring/blog/domain/Category.java
+++ src/main/java/com/spring/blog/domain/Category.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
@@ -15,4 +18,7 @@ public class Category {
 
     @Column(nullable = false)
     private String categoryName;
+
+    @OneToMany
+    private List<Board> board = new ArrayList<Board>();
 }
